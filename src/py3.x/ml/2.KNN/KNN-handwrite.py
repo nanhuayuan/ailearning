5cf5a2db7e247aeae278dc74134371246e4842e4
宋凯 <2044233868@qq.com>
2023-06-05 18:55:17 +0800

    约会、手写识别分开

--- src/py3.x/ml/2.KNN/KNN-handwrite.py
+++ src/py3.x/ml/2.KNN/KNN-handwrite.py
@@ -1,35 +1,64 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-'''
-Created on Sep 16, 2010
-Update  on 2017-05-18
-Author: Peter Harrington/羊三/小瑶
-GitHub: https://github.com/apachecn/AiLearning
-'''
-
-# 导入科学计算包numpy和运算符模块operator
 from numpy import *
 import operator
 import os
-from collections import Counter
 
-
-def createDataSet():
+def handwritingClassTest():
     """
     Desc:
-        创建数据集和标签
+        手写数字识别分类器，并将分类错误数和分类错误率打印出来
     Args:
         None
     Returns:
-        group -- 训练数据集的 features
-        labels -- 训练数据集的 labels
-    调用方式
-    import kNN
-    group, labels = kNN.createDataSet()
+        None
     """
-    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B']
-    return group, labels
+    # 1. 导入数据
+    hwLabels = []
+    trainingFileList = os.listdir("D:\\temp\data-master\\机器学习\\2.KNN\\trainingDigits") # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    # hwLabels存储0～9对应的index位置， trainingMat存放的每个位置对应的图片向量
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        # 将 32*32的矩阵->1*1024的矩阵
+        trainingMat[i] = img2vector('D:\\temp\\data-master\\机器学习\\2.KNN\\trainingDigits\%s' % fileNameStr)
+
+    # 2. 导入测试数据
+    testFileList = os.listdir('D:\\temp\\data-master\\机器学习\\2.KNN\\testDigits')  # iterate through the test set
+    errorCount = 0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('D:\\temp\\data-master\\机器学习\\2.KNN\\testDigits\\%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        errorCount += classifierResult != classNumStr
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / mTest))
+
+def img2vector(filename):
+    """
+    Desc:
+        将图像数据转换为向量
+    Args:
+        filename -- 图片文件 因为我们的输入数据的图片格式是 32 * 32的
+    Returns:
+        returnVect -- 图片文件处理完成后的一维矩阵
+
+    该函数将图像转换为向量: 该函数创建 1 * 1024 的NumPy数组，然后打开给定的文件，
+    循环读出文件的前32行，并将每行的头32个字符值存储在NumPy数组中，最后返回数组。
+    """
+    returnVect = zeros((1, 1024))
+    fr = open(filename, 'r')
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
 
 
 def classify0(inX, dataSet, labels, k):
@@ -114,13 +143,13 @@ def classify0(inX, dataSet, labels, k):
     # b=sorted(a,key=opertator.itemgetter(1,0)) >>>b=[('c',0),('a',1),('b',2)] 这个是先比较第2个元素，然后对第一个元素进行排序，形成多级排序。
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
-    
+
     # ------------------------------------------------------------------------------------------------------------------------------------------
     # 实现 classify0() 方法的第二种方式
 
     # """
     # 1. 计算距离
-    
+
     # 欧氏距离:  点到点之间的距离
     #    第一行:  同一个点 到 dataSet的第一个点的距离。
     #    第二行:  同一个点 到 dataSet的第二个点的距离。
@@ -129,41 +158,32 @@ def classify0(inX, dataSet, labels, k):
 
     # [[1,2,3],[1,2,3]]-[[1,2,3],[1,2,0]]
     # (A1-A2)^2+(B1-B2)^2+(c1-c2)^2
-    
+
     # inx - dataset 使用了numpy broadcasting，见 https://docs.scipy.org/doc/numpy-1.13.0/user/basics.broadcasting.html
     # np.sum() 函数的使用见 https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.sum.html
     # """
-	#   dist = np.sum((inx - dataset)**2, axis=1)**0.5
-    
-    # """
-    # 2. k个最近的标签
-    
-    # 对距离排序使用numpy中的argsort函数， 见 https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.sort.html#numpy.sort
-    # 函数返回的是索引，因此取前k个索引使用[0 : k]
-    # 将这k个标签存在列表k_labels中
-    # """
-    # k_labels = [labels[index] for index in dist.argsort()[0 : k]]
-	# """
-    # 3. 出现次数最多的标签即为最终类别
-    
-    # 使用collections.Counter可以统计各个标签的出现次数，most_common返回出现次数最多的标签tuple，例如[('lable1', 2)]，因此[0][0]可以取出标签值
-	# """
-    # label = Counter(k_labels).most_common(1)[0][0]
-    # return label
-
-    # ------------------------------------------------------------------------------------------------------------------------------------------
 
 
-def test1():
-    """
-    第一个例子演示
-    """
-    group, labels = createDataSet()
-    print(str(group))
-    print(str(labels))
-    print(classify0([0.1, 0.1], group, labels, 3))
+#   dist = np.sum((inx - dataset)**2, axis=1)**0.5
 
+# """
+# 2. k个最近的标签
+
+# 对距离排序使用numpy中的argsort函数， 见 https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.sort.html#numpy.sort
+# 函数返回的是索引，因此取前k个索引使用[0 : k]
+# 将这k个标签存在列表k_labels中
+# """
+# k_labels = [labels[index] for index in dist.argsort()[0 : k]]
+# """
+# 3. 出现次数最多的标签即为最终类别
+
+# 使用collections.Counter可以统计各个标签的出现次数，most_common返回出现次数最多的标签tuple，例如[('lable1', 2)]，因此[0][0]可以取出标签值
+# """
+# label = Counter(k_labels).most_common(1)[0][0]
+# return label
+
+# ------------------------------------------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
-    test1()
+    handwritingClassTest()
